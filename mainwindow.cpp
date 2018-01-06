ee60793e8a9ca7edfcf928a03fd4dea267caa02e
mgorce <mael_gorce@hotmail.fr>
2018-01-06 19:06:56 +0100

    Ajout de la Macro FORALLCELLS dans Mainwindow pour augmenter la lisibilité

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,47 +6,40 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     setFixedSize(C_LAR_WD, C_HAU_WD);
 
-    for (int iI=0; iI < C_LAR_WD/C_SIZE_CEL; iI++)
-    {
-        for (int iJ=0; iJ < C_HAU_WD/C_SIZE_CEL; iJ++)
-        {
-            m_pCCelluleMap[iI][iJ]=new CCellule(iI,iJ,this);
-        }
-    }
+
+    FORALLCELLS
+    (
+        m_pCCelluleMap[iI][iJ]=new CCellule(iI,iJ,this);
+    )
+
 
     CCellule* pCVoisinage[C_NB_VOISINS];
     int pucVoisCorrX[C_NB_VOISINS] = {-1,0,1,-1,1,-1,0,1};
     int pucVoisCorrY[C_NB_VOISINS] = {-1,-1,-1,0,0,1,1,1};
 
-    for (int iI=0; iI < C_LAR_WD/C_SIZE_CEL; iI++)
-    {
-        for (int iJ=0; iJ < C_HAU_WD/C_SIZE_CEL; iJ++)
-        {
-            for(int iV=0; iV < C_NB_VOISINS;iV++)
-            {
-                (iI+pucVoisCorrX[iV]>=0 && \
-                 iI+pucVoisCorrX[iV]<C_LAR_WD/C_SIZE_CEL && \
-                 iJ+pucVoisCorrY[iV]>=0 && \
-                 iJ+pucVoisCorrY[iV]<C_HAU_WD/C_SIZE_CEL ) ? \
+    FORALLCELLS
+    (
+        for(int iV=0; iV < C_NB_VOISINS;iV++)                                               \
+            {                                                                               \
+                (iI+pucVoisCorrX[iV]>=0 &&                                                  \
+                 iI+pucVoisCorrX[iV]<C_LAR_WD/C_SIZE_CEL &&                                 \
+                 iJ+pucVoisCorrY[iV]>=0 &&                                                  \
+                 iJ+pucVoisCorrY[iV]<C_HAU_WD/C_SIZE_CEL ) ?                                \
                  pCVoisinage[iV]=m_pCCelluleMap[iI+pucVoisCorrX[iV]][iJ+pucVoisCorrY[iV]] : \
-                 pCVoisinage[iV]=nullptr;
-            }
-            m_pCCelluleMap[iI][iJ]->fnSetVoisinage(pCVoisinage);
-        }
-    }
+                 pCVoisinage[iV]=nullptr;                                                   \
+            }                                                                               \
+        m_pCCelluleMap[iI][iJ]->fnSetVoisinage(pCVoisinage);                                \
+    )
 
     // Création du Timer et
     m_pQTickerCompute   = new QTimer(this);
     m_pQTickerActualize = new QTimer(this);
 
-    for (int iI=0; iI < C_LAR_WD/C_SIZE_CEL; iI++)
-    {
-        for (int iJ=0; iJ < C_HAU_WD/C_SIZE_CEL; iJ++)
-        {
-           connect(m_pQTickerCompute  , SIGNAL(timeout()), m_pCCelluleMap[iI][iJ], SLOT(fnCompute()));
-           connect(m_pQTickerActualize, SIGNAL(timeout()), m_pCCelluleMap[iI][iJ], SLOT(fnActualize()));
-        }
-    }
+    FORALLCELLS
+    (
+       connect(m_pQTickerCompute  , SIGNAL(timeout()), m_pCCelluleMap[iI][iJ], SLOT(fnCompute()));
+       connect(m_pQTickerActualize, SIGNAL(timeout()), m_pCCelluleMap[iI][iJ], SLOT(fnActualize()));
+    )
 
     m_pQTickerActualize->start(100);
     m_pQTickerCompute->start(100);
--- mainwindow.h
+++ mainwindow.h
@@ -8,6 +8,14 @@
 #define C_LAR_WD 800
 #define C_HAU_WD 500
 
+#define FORALLCELLS(X) \
+    for (int iI=0; iI < C_LAR_WD/C_SIZE_CEL; iI++)              \
+    {                                                           \
+        for (int iJ=0; iJ < C_HAU_WD/C_SIZE_CEL; iJ++)          \
+        {                                                       \
+            X                                                   \
+        }                                                       \
+    }                                                           \
 
 namespace Ui {
 class MainWindow;
